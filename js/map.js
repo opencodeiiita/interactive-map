feffb919a71d0888fe4ef8f059c77d2957533d9c
Onkar Telange <Onkartelange2000@gmail.com>
2020-01-17 19:47:46 +0530

    added files

---
+++ js/map.js
@@ -0,0 +1,34 @@
+
+        
+jQuery(document).ready(function () {
+    $('.title-blue').mouseout(function(){
+        $("#map").attr('src', 'img/original.png');
+    }),
+    $('.title-blue').mouseover( function(){
+        $("#map").attr('src', 'img/office.png');
+    });
+    $('.title-red').mouseout(function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+    }),
+    $('.title-red').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-red.png');
+    });
+    $('.title-green').mouseout(function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+    }),
+    $('.title-green').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-green.png');
+    });	
+    $('.title-yellow').mouseout(function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+    }),
+    $('.title-yellow').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-yellow.png');
+    });
+    $('.title-pink').mouseout(function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+    }),
+    $('.title-pink').mouseover( function(){
+        $("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-pink.png');
+    });	
+});					
